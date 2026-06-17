2dae52093c72346f47c2a4cbbdc4449a7674985c
JeanHaro <JeanCHL98@hotmail.com>
2026-06-17 03:10:31 -0500

    Añadiendo box-shadow al panel de notificaciones, al search en mobile y las notificaciones añadiendo un hostListener

--- src/app/dashboard/layout/header/header.ts
+++ src/app/dashboard/layout/header/header.ts
@@ -1,4 +1,13 @@
-import { Component, inject, input, output, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  inject,
+  input,
+  output,
+  signal,
+  viewChild
+} from '@angular/core';
 
 import {
   IconDefinition,
@@ -18,20 +27,27 @@ import { NotificationService } from './services/notification';
   styleUrl: './header.scss',
 })
 export class Header {
-  // Inyecciones
+  // TODO: ViewChild
+  readonly notificationRef = viewChild(
+    'notificationRef',
+    { read: ElementRef<HTMLElement> }
+  );
+  readonly searchMobileRef = viewChild<ElementRef<HTMLElement>>('searchMobileRef');
+
+  // TODO: Inyecciones
   private readonly notificationService = inject(NotificationService);
 
-  // Iconoss
+  // TODO: Iconoss
   readonly faMagnifyingGlass: IconDefinition = faMagnifyingGlass;
   readonly faSun: IconDefinition = faSun;
   readonly faMoon: IconDefinition = faMoon;
   readonly faBell: IconDefinition = faBell;
 
-  // Propiedades de salida y entrada
+  // TODO: Input y Output
   sidebarActive = output<Event>();
   showSidebar = input<boolean>(false);
 
-  // Propiedades
+  // TODO: Signals
   showSearch = signal<boolean>(false);
   showNotification = signal<boolean>(false);
   isDarkMode = signal<boolean>(false);
@@ -39,24 +55,53 @@ export class Header {
   // Exponemos el servicio al template (solo lectura)
   readonly unreadNotificationCount = this.notificationService.unreadCount;
 
-  // Métodos
+  // TODO: HostListener
+  // ====================================================== MOSTRAR NOTIFICACIONES
+  // Escucha cualquier click en el documento
+  @HostListener('document:click', ['$event'])
+  onHeaderClick ( event: Event ): void {
+    if ( this.showNotification() ) {
+      const notifications = this.notificationRef()?.nativeElement;
+
+      if ( notifications && !notifications.contains(event.target as Node) ) {
+        this.showNotification.set(false);
+      }
+    }
+
+    if ( this.showSearch() ) {
+      const searchMobile = this.searchMobileRef()?.nativeElement;
+
+      if ( searchMobile && !searchMobile.contains(event.target as Node) ) {
+        if (window.innerWidth < 768) this.showSearch.set(false);
+      }
+    }
+  }
+
+
+  // TODO: MÉTODOS
+  // ====================================================== MOSTRAR SIDEBAR
   openSidebar (e: Event): void {
     this.sidebarActive.emit(e);
   }
 
-  openSearch () {
+  // ====================================================== MOSTRAR SEARCH
+  openSearch ( event: Event ): void {
+    event.stopPropagation();
+
     if (window.innerWidth < 768) {
       this.showSearch.set(!this.showSearch());
-
       this.showNotification.set(false);
     }
   }
 
+  // ====================================================== DARK MODE
   toggleDarkMode() {
     this.isDarkMode.set(!this.isDarkMode());
   }
 
-  openNotifications() {
+  // ====================================================== MOSTRAR NOTIFICACIONES
+  openNotifications ( event: Event ): void {
+    event.stopPropagation();
     this.showNotification.set(!this.showNotification())
 
     if (window.innerWidth < 768) this.showSearch.set(false)
