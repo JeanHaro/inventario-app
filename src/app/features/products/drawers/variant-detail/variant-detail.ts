52aa68efc7fb08cfef861b9743e0bd323f5f8fb9
JeanHaro <JeanCHL98@hotmail.com>
2026-06-28 15:27:31 -0500

    Mostrando los datos de la variante correspondiente en el variant-detail

--- src/app/features/products/drawers/variant-detail/variant-detail.ts
+++ src/app/features/products/drawers/variant-detail/variant-detail.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, ElementRef, HostListener, input, output, signal, viewChild } from '@angular/core';
 
 // FontAwesome
 import {
@@ -28,6 +28,9 @@ import { Variant } from '../../models/products.model';
   styleUrl: './variant-detail.scss',
 })
 export class VariantDetail {
+  // TODO: VIEWCHILD
+  readonly optionsMenuRef = viewChild<ElementRef<HTMLElement>>('optionsMenuRef');
+
   // TODO: ICONOS
   readonly faArrowRightFromBracket: IconDefinition = faArrowRightFromBracket;
   readonly faPenToSquare: IconDefinition = faPenToSquare;
@@ -45,5 +48,33 @@ export class VariantDetail {
 
   // TODO: INPUT Y OUTPUT
   readonly variant = input.required<Variant>();
+  readonly productName = input.required<string>();
   readonly closeModal = output<void>();
+
+  // TODO: SIGNALS
+  showOptionsMenu = signal<boolean>(false);
+
+  // TODO: HOSTLISTENER
+  // ====================================================== MOSTRAR OPCIONES
+
+  // Escucha cualquier click en el documento
+  @HostListener('document:click', ['$event'])
+  onDocumentClick ( event: Event ): void {
+    if ( this.showOptionsMenu() ) {
+      const menuOptions = this.optionsMenuRef()?.nativeElement;
+
+      if ( menuOptions && !menuOptions.contains(event.target as Node) ) {
+        this.showOptionsMenu.set(false);
+      }
+    }
+  }
+
+  // TODO: MÉTODOS PÚBLICOS
+  // ====================================================== MOSTRAR OPCIONES
+
+  // Mostrar y ocultar el menu de opciones
+  toggleOptionsMenu ( event: Event ): void {
+    event.stopPropagation();
+    this.showOptionsMenu.set(!this.showOptionsMenu());
+  }
 }
