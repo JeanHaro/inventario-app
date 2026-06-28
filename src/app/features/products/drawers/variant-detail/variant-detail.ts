eb098614fc52408b9159b00507ee7ed3a0ecab89
JeanHaro <JeanCHL98@hotmail.com>
2026-06-28 01:52:54 -0500

    Estructura y estilización del variant-detail completado

--- src/app/features/products/drawers/variant-detail/variant-detail.ts
+++ src/app/features/products/drawers/variant-detail/variant-detail.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
 
+// FontAwesome
+import {
+  faArrowRightFromBracket,
+  faBarcode,
+  faBoxArchive,
+  faBoxesStacked,
+  faClose,
+  faEllipsis,
+  faFileArrowDown,
+  faFloppyDisk,
+  faPenToSquare,
+  faPlus,
+  faSpinner,
+  faTag,
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
-  selector: 'app-variant-detail',
+  selector: 'variant-detail',
   standalone: false,
   templateUrl: './variant-detail.html',
   styleUrl: './variant-detail.scss',
 })
-export class VariantDetail {}
+export class VariantDetail {
+  // TODO: ICONOS
+  readonly faArrowRightFromBracket: IconDefinition = faArrowRightFromBracket;
+  readonly faPenToSquare: IconDefinition = faPenToSquare;
+  readonly faEllipsis: IconDefinition = faEllipsis;
+  readonly faFloppyDisk: IconDefinition = faFloppyDisk;
+  readonly faClose: IconDefinition = faClose;
+  readonly faFileArrowDown: IconDefinition = faFileArrowDown;
+  readonly faBoxArchive: IconDefinition = faBoxArchive;
+  readonly faBarcode: IconDefinition = faBarcode;
+  readonly faSpinner: IconDefinition = faSpinner;
+  readonly faBoxesStacked: IconDefinition = faBoxesStacked;
+  readonly faTag: IconDefinition = faTag;
+  readonly faPlus: IconDefinition = faPlus;
+}
